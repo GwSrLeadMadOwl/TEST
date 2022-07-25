ecc8e0f603e6963e6740be6c53f8e5c1574e8cd1
GwSrLeadMadOwl <gwsrmadowl@gmail.com>
2022-07-25 17:08:40 +0300

    FindLargest.fixed2

--- TABootcampJavaPart/src/com/bootcampexcercise/module4/activity/FindLargest.java
+++ TABootcampJavaPart/src/com/bootcampexcercise/module4/activity/FindLargest.java
@@ -2,13 +2,13 @@ package com.bootcampexcercise.module4.activity;
 
 public class FindLargest {
     public static void main(String args[]){
-        double num1 = Math.random();
-        double num2 = Math.random();
+        double num1 = Math.random() * 100;
+        double num2 = Math.random() * 100;
         if(num1>num2){
-            System.out.println(num1 + ">"+num2);
+            System.out.println((int) num1 + ">"+(int) num2);
 
         }else{
-            System.out.println(num1 + "<"+num2);
+            System.out.println((int) num1 + "<"+(int) num2);
         }
     }
 }
