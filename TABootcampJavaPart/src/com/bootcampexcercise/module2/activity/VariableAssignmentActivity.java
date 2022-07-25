5a46e5738351f13fe350c1697d916add2cc29729
GwSrLeadMadOwl <gwsrmadowl@gmail.com>
2022-07-25 16:27:02 +0300

    activity2

--- TABootcampJavaPart/src/com/bootcampexcercise/module2/activity/VariableAssignmentActivity.java
+++ TABootcampJavaPart/src/com/bootcampexcercise/module2/activity/VariableAssignmentActivity.java
@@ -2,11 +2,27 @@ package com.bootcampexcercise.module2.activity;
 
 public class VariableAssignmentActivity {
     public static void main(String args[]){
-        int a,b,c;
-        a = 10;
-        b = 2;
-        c = 3;
-        a = 12;
-        System.out.println("Result of (" + a + " + " + b + ") * " + c + " is " + ((a+b)*c));
+        sum(10,15);
+//        method2();
+//        method3();
+//        int a = 10;
+//        int b,c;
+//        b = 2;
+//        c = 3;
+//        a = 12;
+//        System.out.println("Result of (" + a + " + " + b + ") * " + c + " is " + ((a+b)*c));
     }
+
+    public static void sum(int a, int b){
+        System.out.println(a + " + " + b);
+        System.out.println(a+b);
+    }
+
+//    public static void method2(){
+//
+//    }
+//
+//    public static void method3(){
+//
+//    }
 }
