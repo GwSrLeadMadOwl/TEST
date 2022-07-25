e068064912a990d2e83ff5e44f39a7c855a11051
GwSrLeadMadOwl <gwsrmadowl@gmail.com>
2022-07-25 16:31:38 +0300

    OperatorActivity

---
+++ TABootcampJavaPart/src/com/bootcampexcercise/module3/activity/OperatorActivity.java
@@ -0,0 +1,15 @@
+package com.bootcampexcercise.module3.activity;
+
+public class OperatorActivity {
+    public static void main(String args[]){
+        int a=10;
+        int b=15;
+
+        sum(a,b);
+    }
+
+    public static void sum(int a, int b){
+        System.out.println(a + " + " + b);
+        System.out.println(a+b);
+    }
+}
