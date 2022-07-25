8202311be3a4c87552372f115fb36ed840862358
GwSrLeadMadOwl <gwsrmadowl@gmail.com>
2022-07-25 16:59:19 +0300

    FindLargest

---
+++ TABootcampJavaPart/src/com/bootcampexcercise/module4/activity/FindLargest.java
@@ -0,0 +1,14 @@
+package com.bootcampexcercise.module4.activity;
+
+public class FindLargest {
+    public static void main(String args[]){
+        double num = Math.random();
+        if(num>0.5){
+            System.out.println("Big number");
+
+        }else{
+            System.out.println("Small number");
+        }
+        System.out.println(num);
+    }
+}
