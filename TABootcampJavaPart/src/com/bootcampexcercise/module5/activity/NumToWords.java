c6ebf3cbf8ce30fb309e202cc7255321821184ae
GwSrLeadMadOwl <gwsrmadowl@gmail.com>
2022-07-25 17:47:43 +0300

    NumToWords.fixed

--- TABootcampJavaPart/src/com/bootcampexcercise/module5/activity/NumToWords.java
+++ TABootcampJavaPart/src/com/bootcampexcercise/module5/activity/NumToWords.java
@@ -2,40 +2,43 @@ package com.bootcampexcercise.module5.activity;
 
 public class NumToWords {
     public static void main(String args[]){
-        int x = 5;
+        int x = 11;
+        String numText = "";
         switch(x){
             case 1:
-                System.out.println("one");
+                numText = "one";
                 break;
             case 2:
-                System.out.println("two");
+                numText = "two";
                 break;
             case 3:
-                System.out.println("three");
+                numText = "three";
                 break;
             case 4:
-                System.out.println("four");
+                numText = "four";
                 break;
             case 5:
-                System.out.println("five");
+                numText = "five";
                 break;
             case 6:
-                System.out.println("six");
+                numText = "six";
                 break;
             case 7:
-                System.out.println("seven");
+                numText = "seven";
                 break;
             case 8:
-                System.out.println("eight");
+                numText = "eight";
                 break;
             case 9:
-                System.out.println("nine");
+                numText = "nine";
                 break;
             case 10:
-                System.out.println("ten");
+                numText = "ten";
                 break;
             default:
-                System.out.println("implementation to support numbers 1-10");;
+//                System.out.println("implementation to support numbers 1-10");
+                numText = "Number " + x;
         }
+        System.out.println(numText);
     }
 }
