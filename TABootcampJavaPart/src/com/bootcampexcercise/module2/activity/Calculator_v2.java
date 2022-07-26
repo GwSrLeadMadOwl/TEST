9363c9e479bf2bc0a2e42370ed3f86d3d4d63b98
GwSrLeadMadOwl <gwsrmadowl@gmail.com>
2022-07-26 11:49:47 +0300

    calculator_v2

---
+++ TABootcampJavaPart/src/com/bootcampexcercise/module2/activity/Calculator_v2.java
@@ -0,0 +1,30 @@
+package com.bootcampexcercise.module2.activity;
+
+public class Calculator_v2 {
+    public static void main(String args[]){
+        int a=10;
+        int b=5;
+
+        System.out.println(addition(a,b));
+        System.out.println(subtraction(a,b));
+        System.out.println(multiplication(a,b));
+        System.out.println(division(a,b));
+    }
+
+    public static int addition(int a, int b){
+        return (a+b);
+
+    }
+
+    public static int subtraction(int a, int b){
+        return (a-b);
+    }
+
+    public static int multiplication(int a, int b){
+        return (a*b);
+    }
+
+    public static int division(int a, int b){
+        return (a/b);
+    }
+}
