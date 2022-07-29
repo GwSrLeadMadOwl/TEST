146eaab092d8469e5884a119ff06399c7f60ffa1
GwSrLeadMadOwl <gwsrmadowl@gmail.com>
2022-07-29 17:12:49 +0300

    Exam.Completed

--- TABootcampJavaPart/src/com/bootcampexcercise/module10/activity3/Calculator2.java
+++ TABootcampJavaPart/src/com/bootcampexcercise/module10/activity3/Calculator2.java
@@ -7,19 +7,19 @@ public class Calculator2  extends Throwable {
         Calculator2 cal = new Calculator2();
         CustomizedException ce = new CustomizedException();
 
-        cal.multiply(10, 2);
+//        cal.multiply(10, 2);
         cal.multiply(100, 200);
     }
 
     double multiply(double a, double b) throws CustomizedException{
         double res = 0;
         try {
-            return res =(a * b);
+            res =(a * b);
             if(res>=2000){ //Unreachable statement
                 throw new CustomizedException();
             }
         }catch(CustomizedException ce){
-            ce.getMessage();
+            System.out.println(ce.getMessage());
         }finally {
             if(res>0 && res<2000){
                 System.out.println(res);
